28544ba041ae5ccea8d72d8309049a8fd8496a53
Yoann BOUTIN <yo-java@hotmail.fr>
2015-03-15 15:05:06 +0100

    join party

--- rejoindrePartie.js
+++ rejoindrePartie.js
@@ -9,14 +9,27 @@ socket.on('listGame', function(tab){
 
 function showJoin(){
     var i;
+    var j;
     $('#joinrooms').empty();
     for(i = 0; i < allParty.length; i++){
-        $('#joinrooms').append('<div><a href="#" onclick="switchParty(' + allParty[i] + ')">' + allParty[i][0] + '</a></div>');
+        $('#joinrooms').append('<div><a href="#" onclick="switchParty(\'' + allParty[i][0] + '\')">' + allParty[i][0] + '</a></div>');
+        //$('#joinrooms').append('<div><a href="#" onclick="switchParty(0)">' + allParty[i][0] + '</a></div>');
     }
+    /*$.each(allParty, function(key, value) {
+         //$('#joinrooms').append('<div><a href="#" onclick="switchRoom(\'' + allParty[i] + '\')">' + value[key] + '</a></div>');
+        $('#joinrooms').append('<div><a href="#" onclick="alert(0);">' + value[key] + '</a></div>');
+    });*/
 }
 
 
 function switchParty(party){
-    window.alert("bob");
-    socket.emit('switchJoin', party);
+    var i;
+    alert(party);
+    for(i=0;i<allParty.length;i++){
+        if(party === allParty[i][0]){
+            socket.emit('switchJoin', allParty[i]);
+        }
+    }
+    boolJoin = false;
+
 }
